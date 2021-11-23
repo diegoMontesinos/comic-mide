b062ac56e7eb674ac3d85ead4d8b1b70244ff328
Diego Montesinos <chesp.gommont@gmail.com>
2021-11-23 14:55:58 -0600

    Responsive home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import MainTitleImg from '../../assets/main-title.png';
-import MainTitleLogo from '../../assets/main-title-logo.svg';
+import MainTitleLogoImg from '../../assets/main-title-logo.svg';
+import ChangePortraitImg from '../../assets/change-portrait.svg';
 
 import './index.scss';
 
@@ -13,12 +14,26 @@ const HomePage: React.FC = () => (
       <img src={MainTitleImg} alt="Main title" />
     </div>
     <div className="mide-text welcome-text">
-      <span>Bienvenido al primer Comic Digital del MIDE</span>
+      <span>
+        Bienvenido al primer Comic Digital del MIDE
+      </span>
+    </div>
+    <div className="welcome-portrait-img">
+      <img src={ChangePortraitImg} alt="Change Portrait" />
+    </div>
+    <div className="mide-text welcome-portrait-text">
+      <span>
+        Te recomendamos girar el teléfono de forma
+        horizontal para una mejor experiencia.
+      </span>
     </div>
     <div className="mide-title-logo">
-      <img src={MainTitleLogo} alt="Main Title Logo" />
+      <img src={MainTitleLogoImg} alt="Main Title Logo" />
     </div>
-    <Link to="/tutorial" className="mide-text primary-btn main-title-btn">
+    <Link
+      to="/tutorial"
+      className="mide-text primary-btn main-title-btn"
+    >
       <span>EMPEZAR</span>
     </Link>
   </div>
