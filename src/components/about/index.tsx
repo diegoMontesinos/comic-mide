408e48bfba15050dd7192b08c06c12c56a2e9190
Diego Montesinos <chesp.gommont@gmail.com>
2021-11-23 20:59:30 -0600

    Add responsive to about

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -8,257 +8,107 @@ const AboutPage: React.FC = () => (
   <div className="about">
     <h3 className="about-title mide-text">CRÉDITOS</h3>
     <div className="about-content">
-      <div className="about-content-column">
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Curabitur ornare, nisi ac mollis tincidunt
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Tellus arcu pulvinar lacus, a placerat
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Ante tortor nec risus
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Donec euismod Urna vel est dictum placerat vel
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Curabitur ornare, nisi ac mollis tincidunt
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Tellus arcu pulvinar lacus, a placerat
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Ante tortor nec risus
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Donec euismod Urna vel est dictum placerat vel
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Curabitur ornare, nisi ac mollis tincidunt
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Tellus arcu pulvinar lacus, a placerat
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Ante tortor nec risus
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Donec euismod Urna vel est dictum placerat vel
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-      </div>
-
-      <div className="about-content-column">
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Curabitur ornare, nisi ac mollis tincidunt
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Tellus arcu pulvinar lacus, a placerat
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Ante tortor nec risus
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Donec euismod Urna vel est dictum placerat vel
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Curabitur ornare, nisi ac mollis tincidunt
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Tellus arcu pulvinar lacus, a placerat
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Ante tortor nec risus
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Donec euismod Urna vel est dictum placerat vel
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Curabitur ornare, nisi ac mollis tincidunt
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Tellus arcu pulvinar lacus, a placerat
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Ante tortor nec risus
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Donec euismod Urna vel est dictum placerat vel
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-      </div>
-
-      <div className="about-content-column">
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Curabitur ornare, nisi ac mollis tincidunt
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Tellus arcu pulvinar lacus, a placerat
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Ante tortor nec risus
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Donec euismod Urna vel est dictum placerat vel
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Curabitur ornare, nisi ac mollis tincidunt
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Tellus arcu pulvinar lacus, a placerat
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Ante tortor nec risus
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Donec euismod Urna vel est dictum placerat vel
-          </div>
-        </p>
-
-        <p className="about-content-item">
-          <div className="about-content-title mide-text">
-            Lorem ipsum dolor sit amet
-          </div>
-          <div className="about-content-subtitle mide-text">
-            Consectetur adipiscing elit
-          </div>
-        </p>
-
-        <div className="about-content-logo">
-          <img src={MIDELogoImg} alt="MIDE Logo" />
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Lorem ipsum dolor sit amet1
         </div>
+        <div className="about-content-subtitle mide-text">
+          Consectetur adipiscing elit
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Curabitur ornare, nisi ac mollis tincidunt2
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Tellus arcu pulvinar lacus, a placerat
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Ante tortor nec risus
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Donec euismod Urna vel est dictum placerat vel
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Lorem ipsum dolor sit amet
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Consectetur adipiscing elit
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Curabitur ornare, nisi ac mollis tincidunt
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Tellus arcu pulvinar lacus, a placerat
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Ante tortor nec risus
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Donec euismod Urna vel est dictum placerat vel
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Lorem ipsum dolor sit amet
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Consectetur adipiscing elit
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Curabitur ornare, nisi ac mollis tincidunt
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Tellus arcu pulvinar lacus, a placerat
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Ante tortor nec risus
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Donec euismod Urna vel est dictum placerat vel
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Lorem ipsum dolor sit amet
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Consectetur adipiscing elit
+        </div>
+      </p>
+
+      <p className="about-content-item">
+        <div className="about-content-title mide-text">
+          Lorem ipsum dolor sit amet
+        </div>
+        <div className="about-content-subtitle mide-text">
+          Consectetur adipiscing elit
+        </div>
+      </p>
+
+      <div className="about-content-logo">
+        <img src={MIDELogoImg} alt="MIDE Logo" />
       </div>
     </div>
   </div>
